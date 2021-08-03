b001fcecee7bdb12418ccabc88e925434bac1404
Kir-Cicegi <84298571+Kir-Cicegi@users.noreply.github.com>
2021-08-03 17:27:15 -0400

    change interests input

--- src/components/ProfileData/ProfileData.jsx
+++ src/components/ProfileData/ProfileData.jsx
@@ -3,8 +3,8 @@ import "./ProfileData.css"
 export default function ProfileData(props) {
     return(
         <>
-            <h2>{props.profileData.bio}</h2>
-            <h2>{props.profileData.interests}</h2>
+            <h2>Bio: {props.profileData.bio}</h2>
+            <h2>Interests: {props.profileData.interests.map(i => <p>{i.label}</p>)}</h2>
             <h2>{props.profileData.friends}</h2>
         </>
     )
