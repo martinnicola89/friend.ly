0d2cd4c7a8a908da107b00f873abe223b50ba225
Mac <mac@Macs-MacBook-Pro.local>
2021-08-03 19:57:04 -0400

    changed address for socket io

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const logger = require('morgan');
 const io = require("socket.io")(8900, {
   cors: {
     // origin: "http://localhost:3000",
-    origin: "friendl-y.herokuapp.com",
+    origin: "https://friendl-y.herokuapp.com",
   },
 })
 
