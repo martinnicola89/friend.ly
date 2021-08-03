985d0d538a0e194b7b94e30d5625b317017b2f3f
Mac <mac@Macs-MacBook-Pro.local>
2021-08-03 17:39:34 -0400

    Deploy

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const logger = require('morgan');
 
 const io = require("socket.io")(8900, {
   cors: {
-    origin: "mongodb+srv://Kircicegi:sei40@mkd.geogs.mongodb.net/test",
+    origin: "http://localhost:3000",
   },
 })
 
@@ -84,7 +84,8 @@ app.get('/*', function(req, res) {
 
 // Configure to use port 3001 instead of 3000 during
 // development to avoid collision with React's dev server
-const port = process.env.PORT || 3001;
+// const port = process.env.PORT || 3001;
+const port = 3000;
 
 app.listen(port, function() {
   console.log(`Express app running on port ${port}`)
