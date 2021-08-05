33dfd397a5173307fb63945af6ff853cbb3b51e7
Mac <mac@Macs-MacBook-Pro.local>
2021-08-05 13:55:58 -0400

    Changed port back to process.env.port in server.js

--- server.js
+++ server.js
@@ -84,8 +84,7 @@ app.get('/*', function(req, res) {
 
 // Configure to use port 3001 instead of 3000 during
 // development to avoid collision with React's dev server
-// const port = process.env.PORT || 433;
-const port = 433;
+const port = process.env.PORT || 433;
 
 server.listen(port, function() {
   console.log(`Express app running on port ${port}`)
